aaa3c3b6565f2d36066df3df5c01cf9830226b55
lizluft-web <liz.luft@aluno.lsb.com.br>
2025-10-28 21:04:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function calcular(){
+   
+    let valorbr = document.getElementById("valorbr").value;
+    let valours = valorbr * 5.403;
+    document.getElementById("valor").innerHTML = valours;
+}
